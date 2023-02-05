35f09d3ff2962489ed43ef33e36aafd2e1ffb6b1
tnp2004 <101316716+tnp2004@users.noreply.github.com>
2023-02-05 16:22:00 +0700

    Clear the last stage sound effect Update

--- src/Components/Cleardisplay.js
+++ src/Components/Cleardisplay.js
@@ -1,11 +1,25 @@
 import { NavLink } from "react-router-dom"
 import arrow from "../Images/down-arrow.png"
+import { Howl } from "howler"
+import congratulationSfx from "../Soundeffects/Congratulations.mp3"
+
+const sfx = {
+    Congratulations: new Howl({
+      src: [
+        congratulationSfx
+      ],
+      volume: 0.3
+    })
+}
 
 const Cleardisplay = () => {
+    
+    sfx.Congratulations.play()
+
     return (
         <div className="mx-auto text-center text-5xl">
-            <h1 className="my-20">Now you have cleared the last stage.</h1>
-            <p>you can back to play any stage.</p>
+            <h1 className="my-20 h-40 font-extrabold text-transparent text-9xl bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">Congratulations</h1>
+            <h1 className="my-20 ">Now you have cleared the last stage.</h1>
             <img className="mx-auto animate-bounce my-12" src={arrow} />
             <NavLink className="inline-block mx-12" to="/">
                 <button className="drop-shadow-xl py-2 px-3 bg-violet-500 hover:bg-violet-600 text-white text-sm font-semibold rounded-md shadow focus:outline-none">Homepage</button>
